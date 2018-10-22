e1c5b96c91f417709a97d036d89c4297a851cde4
Néstor <Nstor@DESKTOP-C3JB9T0>
2018-10-22 12:02:43 +0100

    Creado controlador, añadidos JSP,librerías y cambiado el nombre del contexto.

---
+++ Tarea22Octubre/WebContent/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Listado objetos basicos</title>
+</head>
+<body>
+<h1>Listado objetos basicos</h1>
+<ol>
+	<li> <a href = "Logica">listado sin</a>
+	 <li><a href = "Logica">listado con</a>
+	 
+	 </ol> 
+
+</body>
+</html>
---
+++ Tarea22Octubre/src/control/Controlador.java
@@ -0,0 +1,43 @@
+package control;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Logica")
+public class Controlador extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private Object[] array = new Object[20];
+    private ArrayList<Object> lista = null;
+    private HashMap<String, Object> mapa = null;
+    
+    
+    public Controlador() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
